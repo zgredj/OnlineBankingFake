5921e6a39ff5bc564db72a10ea531fe3a003b0a9
zkappa <zkappa@WS-ZKAPPA.bbcnet-zh.local>
2017-02-01 17:13:42 +0100

    Basis

--- src/Gui/BasisLayout.java
+++ src/Gui/BasisLayout.java
@@ -8,7 +8,7 @@ public class BasisLayout extends JFrame {
 	public BasisLayout() {
 		setTitle("BBC BANK - Online Banking");
 		setVisible(true);
-		setSize(1200, 800);
+		setSize(1100, 800);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout( new BorderLayout());
