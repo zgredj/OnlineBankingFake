2d1aa77e3db9351a20ff06d914e7bb7080282906
zkappa <zkappa@WS-ZKAPPA.bbcnet-zh.local>
2017-02-02 14:28:43 +0100

    df

--- src/Gui/BasisLayout.java
+++ src/Gui/BasisLayout.java
@@ -5,9 +5,13 @@ import java.awt.*;
 import javax.swing.*;
 
 public class BasisLayout extends JFrame {
+	
+	JPanel alles = new JPanel(new BorderLayout());
+	
 	public BasisLayout() {
 		setTitle("BBC BANK - Online Banking");
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		alles.setBorder(BorderFactory.createEmptyBorder(40, 120, 10, 100));
 	}
 }
--- src/Gui/Tester.java
+++ src/Gui/Tester.java
@@ -3,9 +3,10 @@ package Gui;
 public class Tester {
 
 	public static void main(String[] args) {
-		//Login l = new Login();
-		BasisLayoutEingelogt ble = new BasisLayoutEingelogt();
+		Login l = new Login();
+		//BasisLayoutEingelogt ble = new BasisLayoutEingelogt();
 		//Home h = new Home();
+		//BasisLayout bl = new BasisLayout();
 	}
 
 }
