5064eb9dadfd81a82c2c6e97ca37e60ed6d3d831
zkappa <zkappa@WS-ZKAPPA.bbcnet-zh.local>
2017-02-02 12:30:06 +0100

    df

--- src/Gui/Login.java
+++ src/Gui/Login.java
@@ -1,7 +1,6 @@
 package Gui;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
@@ -27,16 +26,27 @@ public class Login extends BasisLayout {
 		alles.add(panelTitel, BorderLayout.NORTH);
 
 		JPanel panelInhalt = new JPanel(new BorderLayout());
-		JTextField textFieldKartennummer = new JTextField();
-		JPanel panelPassLog = new JPanel(new FlowLayout());
-		JTextField textFieldPasswort = new JTextField();
+		JTextField textFieldKartennummer = new JTextField(10);
+		JPanel panelPassLog = new JPanel(new BorderLayout());
+		JTextField textFieldPasswort = new JTextField(10);
 		JButton buttonLogin = new JButton("Login");
-		panelPassLog.add(textFieldPasswort);
-		panelPassLog.add(buttonLogin);
+		JPanel panelLabelRegbutton = new JPanel(new BorderLayout());
+		JLabel labelText = new JLabel("Noch kein Benutzerkonto?");
+		JButton buttonRegistrieren = new JButton("Registrieren");
+
 		panelInhalt.add(textFieldKartennummer, BorderLayout.NORTH);
-		panelInhalt.add(panelPassLog, BorderLayout.SOUTH);
-		
-		alles.add(panelInhalt, BorderLayout.CENTER);
+
+		panelPassLog.add(textFieldPasswort, BorderLayout.WEST);
+		panelPassLog.add(buttonLogin, BorderLayout.EAST);
+
+		panelLabelRegbutton.add(labelText, BorderLayout.NORTH);
+		panelLabelRegbutton.add(buttonRegistrieren, BorderLayout.SOUTH);
+
+		panelInhalt.add(panelPassLog, BorderLayout.CENTER);
+		panelInhalt.add(panelLabelRegbutton, BorderLayout.SOUTH);
+
+		alles.add(panelInhalt, BorderLayout.SOUTH);
 		
 		add(alles, BorderLayout.CENTER);
+	}
 }
