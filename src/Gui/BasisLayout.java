3f44f44f60861dc47321762a1df7ab39a1743731
zgredj <zgredj@WS-ZGREDJ.bbcnet-zh.local>
2017-02-02 12:56:15 +0100

    lol

--- src/Gui/BasisLayout.java
+++ src/Gui/BasisLayout.java
@@ -7,9 +7,12 @@ import javax.swing.*;
 public class BasisLayout extends JFrame {
 	public BasisLayout() {
 		setTitle("BBC BANK - Online Banking");
-		setVisible(true);
 		setSize(1200, 800);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout( new BorderLayout());
+		setVisible(true);
 	}
 }
+
+// BANANANANANANNANANANNAN
--- src/Gui/Registrieren.java
+++ src/Gui/Registrieren.java
@@ -1,7 +1,7 @@
 package Gui;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
@@ -12,18 +12,23 @@ import javax.swing.JTextField;
 public class Registrieren extends BasisLayout {
 
 	public Registrieren() {
-
-		JLabel labelTitel = new JLabel();
+		//JTextField textFieldKartennummer2 = new JTextField("Kartennummer"); 
+		//add(textFieldKartennummer2);
+		JLabel labelTitel = new JLabel("Neues Benutzerkonto");
 		JPanel panelKopfzeile = new JPanel(new BorderLayout());
-		labelTitel.setText("Neues Benutzerkonto");
 		labelTitel.setFont(new Font("Arial", Font.PLAIN, 55));
-		
-		add(panelKopfzeile, BorderLayout.NORTH);
 		panelKopfzeile.setBorder(BorderFactory.createEmptyBorder(100, 230, 0, 0));
-		panelKopfzeile.add(labelTitel, BorderLayout.WEST);
-		
-		JTextField kartennummer = new JTextField("Kartennummer", 15);
-		add(kartennummer, BorderLayout.NORTH);
-		
+		panelKopfzeile.add(labelTitel, BorderLayout.NORTH);
+
+		add(panelKopfzeile, BorderLayout.NORTH);
+
+		JPanel panelInhalt = new JPanel(); // new GridLayout(8, 1)
+		panelInhalt.setBackground(Color.blue);
+
+		JLabel labelTitel2 = new JLabel("Neues Benutzerkonto");
+		// labelTitel.setFont(new Font("Arial", Font.PLAIN, 55));
+		panelInhalt.add(labelTitel2);
+
+		add(panelInhalt, BorderLayout.SOUTH);
 	}
 }
