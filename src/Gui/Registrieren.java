86fbc583271039edd209d2d3b62995ee173dc580
zgredj <zgredj@WS-ZGREDJ.bbcnet-zh.local>
2017-02-02 14:08:44 +0100

    In Registrieren wurden einige Dinge hinzugefügt

--- src/Gui/Registrieren.java
+++ src/Gui/Registrieren.java
@@ -1,6 +1,7 @@
 package Gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -13,7 +14,8 @@ import javax.swing.JTextField;
 public class Registrieren extends BasisLayout {
 
 	public Registrieren() {
-		JTextField textFieldKartennummer = new JTextField("Kartennummer");
+
+		JTextField textFieldKartennummer = new JTextField("Kartennummer", 10);
 		JTextField textFieldPassword = new JTextField("Passwort");
 		JTextField textFieldPasswordWiederholen = new JTextField("Passwort wiederholen");
 		JTextField textFieldVorname = new JTextField("Vorname");
@@ -23,25 +25,39 @@ public class Registrieren extends BasisLayout {
 		JTextField textFieldPlz = new JTextField("PLZ");
 		JTextField textFieldStrasse = new JTextField("Strasse");
 		JTextField textFieldHausNr = new JTextField("HausNr.");
-		
-		
-		
-		
-		
-		JPanel panelInhalt = new JPanel(new GridLayout(8, 1));
-		add(panelInhalt, BorderLayout.CENTER);
+
+		JPanel panelInhalt = new JPanel(new GridLayout(10, 1));
+		panelInhalt.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 1000));
+		JPanel panelInhaltOrt = new JPanel(new BorderLayout());
+		JPanel panelInhaltStrasse = new JPanel(new BorderLayout());
+		add(panelInhalt, BorderLayout.WEST);
+
 		panelInhalt.add(textFieldKartennummer);
+		panelInhalt.add(textFieldPassword);
+		panelInhalt.add(textFieldPasswordWiederholen);
+		panelInhalt.add(textFieldVorname);
+		panelInhalt.add(textFieldNachname);
+		panelInhalt.add(textFieldGeburtsdatum);
+		panelInhalt.add(textFieldWohnort);
+		panelInhalt.add(textFieldPlz);
+		panelInhalt.add(panelInhaltOrt);
+		panelInhalt.add(panelInhaltStrasse);
+
+		panelInhaltOrt.add(textFieldWohnort, BorderLayout.WEST);
+		panelInhaltOrt.add(textFieldPlz, BorderLayout.CENTER);
+
+		panelInhaltStrasse.add(textFieldStrasse, BorderLayout.WEST);
+		panelInhaltStrasse.add(textFieldHausNr, BorderLayout.CENTER);
+
 		JLabel labelTitel = new JLabel("Neues Benutzerkonto");
 		JPanel panelKopfzeile = new JPanel(new BorderLayout());
 		labelTitel.setFont(new Font("Arial", Font.PLAIN, 55));
-		panelKopfzeile.setBorder(BorderFactory.createEmptyBorder(100, 230, 0, 0));
 		panelKopfzeile.add(labelTitel, BorderLayout.NORTH);
 
 		add(panelKopfzeile, BorderLayout.NORTH);
 
-
 		add(panelInhalt, BorderLayout.SOUTH);
-		setSize(1200, 800); 
-		setResizable(false);
+		setSize(1200, 800);
+		setResizable(true);
 	}
 }
