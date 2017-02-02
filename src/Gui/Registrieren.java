4b727ea51be6ce9751311a906f3a72ea1b6cd755
zgredj <zgredj@WS-ZGREDJ.bbcnet-zh.local>
2017-02-02 17:11:59 +0100

    adfasdfasdf

--- src/Gui/Registrieren.java
+++ src/Gui/Registrieren.java
@@ -1,12 +1,13 @@
 package Gui;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.ComponentOrientation;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
-import javax.swing.JFrame;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -15,58 +16,86 @@ public class Registrieren extends BasisLayout {
 
 	public Registrieren() {
 
-		JTextField textFieldKartennummer = new JTextField("Kartennummer", 10);
-		JTextField textFieldPassword = new JTextField("Passwort");
-		JTextField textFieldPasswordWiederholen = new JTextField("Passwort wiederholen");
-		JTextField textFieldVorname = new JTextField("Vorname");
-		JTextField textFieldNachname = new JTextField("Nachname");
-		JTextField textFieldGeburtsdatum = new JTextField("Geburtsdatum");
-		JTextField textFieldWohnort = new JTextField("Wohnort");
-		JTextField textFieldPlz = new JTextField("PLZ");
-		JTextField textFieldStrasse = new JTextField("Strasse");
-		JTextField textFieldHausNr = new JTextField("HausNr.");
-
-		JPanel panelInhalt = new JPanel(new GridLayout(10, 1));
+		JTextField textFieldKartennummer = new JTextField();
+		JTextField textFieldPassword = new JTextField();
+		JTextField textFieldPasswordWiederholen = new JTextField();
+		JTextField textFieldVorname = new JTextField();
+		JTextField textFieldNachname = new JTextField();
+		JTextField textFieldGeburtsdatum = new JTextField();
+		JTextField textFieldWohnort = new JTextField();
+		JTextField textFieldPlz = new JTextField();
+		JTextField textFieldStrasse = new JTextField();
+		JTextField textFieldHausNr = new JTextField();
+		
+		JLabel labelTitel = new JLabel("Neues Benutzerkonto");
+		
+		JLabel stringKartennummer = new JLabel("Kartennummer");
+		JLabel stringPassword = new JLabel("Passwort");
+		JLabel stringPasswordWiederholen = new JLabel("Passwort wiederholen");
+		JLabel stringVorname = new JLabel("Vorname");
+		JLabel stringNachname = new JLabel("Nachname");
+		JLabel stringGeburtsdatum = new JLabel("Geburtsdatum");
+		JLabel stringWohnort = new JLabel("Wohnort");
+		JLabel stringPlz = new JLabel("PLZ");
+		JLabel stringStrasse = new JLabel("Strasse");
+		JLabel stringHausNr = new JLabel("Haus Nr.");
+		JLabel stringLeer = new JLabel(" ");
+		
+		JPanel panelKopfzeile = new JPanel(new BorderLayout());
+		
+		JPanel panelInhalt = new JPanel(new GridLayout(11, 2, 30, 15));
 		JPanel panelInhaltOrt = new JPanel(new BorderLayout());
 		JPanel panelInhaltStrasse = new JPanel(new BorderLayout());
-		alles.add(panelInhalt, BorderLayout.CENTER);
-
-		panelInhalt.add(textFieldKartennummer);
-		panelInhalt.add(textFieldPassword);
-		panelInhalt.add(textFieldPasswordWiederholen);
-		panelInhalt.add(textFieldVorname);
-		panelInhalt.add(textFieldNachname);
-		panelInhalt.add(textFieldGeburtsdatum);
-		panelInhalt.add(textFieldWohnort);
-		panelInhalt.add(textFieldPlz);
-		panelInhalt.add(panelInhaltOrt);
-		panelInhalt.add(panelInhaltStrasse);
+		JPanel panelOsten = new JPanel(new BorderLayout());
 		
-		textFieldKartennummer;
-		textFieldPassword;
-		textFieldPasswordWiederholen;
-		textFieldVorname;
-		textFieldNachname;
-		textFieldGeburtsdatum;
-		textFieldWohnort;
-		textFieldPlz;
-		text
+		JButton buttonRegistrieren = new JButton("registrieren");
 
-		panelInhaltOrt.add(textFieldWohnort, BorderLayout.WEST);
-		panelInhaltOrt.add(textFieldPlz, BorderLayout.CENTER);
+		panelInhalt.add(stringKartennummer);
+		panelInhalt.add(textFieldKartennummer);
+		
+		panelInhalt.add(stringPassword);
+		panelInhalt.add(textFieldPassword);
+		
+		panelInhalt.add(stringPasswordWiederholen);
+		panelInhalt.add(textFieldPasswordWiederholen);
+		
+		panelInhalt.add(stringVorname);
+		panelInhalt.add(textFieldVorname);
+		
+		panelInhalt.add(stringNachname);
+		panelInhalt.add(textFieldNachname);
+		
+		panelInhalt.add(stringGeburtsdatum);
+		panelInhalt.add(textFieldGeburtsdatum);
+		
+		panelInhalt.add(stringWohnort);
+		panelInhalt.add(textFieldWohnort);
+		
+		panelInhalt.add(stringPlz);
+		panelInhalt.add(textFieldPlz);
+		
+		panelInhalt.add(stringStrasse);
+		panelInhalt.add(textFieldStrasse);
+		
+		panelInhalt.add(stringHausNr);
+		panelInhalt.add(textFieldHausNr);
+		
+		panelInhalt.add(stringLeer);
+		panelInhalt.add(buttonRegistrieren);
 
-		panelInhaltStrasse.add(textFieldStrasse, BorderLayout.WEST);
-		panelInhaltStrasse.add(textFieldHausNr, BorderLayout.CENTER);
-
-		JLabel labelTitel = new JLabel("Neues Benutzerkonto");
-		JPanel panelKopfzeile = new JPanel(new BorderLayout());
+		
+		
 		labelTitel.setFont(new Font("Arial", Font.PLAIN, 55));
-		panelKopfzeile.add(labelTitel, BorderLayout.NORTH);
-
+		
+		alles.add(panelInhalt, BorderLayout.CENTER);
 		alles.add(panelKopfzeile, BorderLayout.NORTH);
-
-		panelKopfzeile.add(panelInhalt, BorderLayout.SOUTH);
-		panelInhalt.setBorder(BorderFactory.createEmptyBorder(40, 5, 10, 800));
+		
+		
+		
+		panelKopfzeile.add(labelTitel, BorderLayout.NORTH);
+		panelKopfzeile.add(panelInhalt, BorderLayout.WEST);
+		panelKopfzeile.add(panelOsten, BorderLayout.EAST);
+		panelInhalt.setBorder(BorderFactory.createEmptyBorder(40, 30, 10, 0));
 		
 		add(alles);
 		setSize(1200, 800);
