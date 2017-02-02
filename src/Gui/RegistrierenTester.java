31ff66ebba7d1bd373d1964b1d390833f3475b1b
zgredj <zgredj@WS-ZGREDJ.bbcnet-zh.local>
2017-02-02 10:13:44 +0100

    Bg

---
+++ src/Gui/RegistrierenTester.java
@@ -0,0 +1,11 @@
+package Gui;
+
+public class RegistrierenTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Registrieren g1 = new Registrieren();
+
+	}
+
+}
