7c3c52f03349869d3a5488dba8a4a0e2585f069c
zkappa <zkappa@WS-ZKAPPA.bbcnet-zh.local>
2017-02-03 12:49:38 +0100

    sdf

--- src/Gui/Login.java
+++ src/Gui/Login.java
@@ -9,55 +9,50 @@ public class Login extends BasisLayout {
 	public Login() {
 
 		JPanel panelSeite = new JPanel();
-		panelSeite.setLayout(new BoxLayout(panelSeite, BoxLayout.PAGE_AXIS));
-
+		panelSeite.setLayout(new BorderLayout());
+		//panelSeite.setLayout(new GridLayout(7, 1));
+		
 		// Titel
 		JPanel panelTitel = new JPanel(new BorderLayout());
 		JLabel labelBbcBank = new JLabel("BBC BANK");
-		labelBbcBank.setFont(new Font("Arial", Font.PLAIN, 70));
+		labelBbcBank.setFont(new Font("Arial", Font.PLAIN, 80));
 		panelTitel.add(labelBbcBank, BorderLayout.CENTER);
 
 		// Login
 		JPanel panelLogin = new JPanel(new BorderLayout());
-		JLabel labelLogin = new JLabel("Login2");
+		JLabel labelLogin = new JLabel("Login");
 		labelLogin.setFont(new Font("Arial", Font.PLAIN, 50));
 		panelLogin.add(labelLogin, BorderLayout.CENTER);
 
 		// Kartennummer
-		JPanel panelKartennummer = new JPanel(new FlowLayout());
-		JTextField textFieldKartennummer = new JTextField(20);
+		JPanel panelKartennummer = new JPanel();
+		JTextField textFieldKartennummer = new JTextField(17);
 		JLabel labelKartennummer = new JLabel("Kartennummer:");
 		panelKartennummer.add(labelKartennummer);
 		panelKartennummer.add(textFieldKartennummer);
 
 		// Passwort
-		JPanel panelPasswortUndLogin = new JPanel(new FlowLayout());
+		JPanel panelPasswort = new JPanel(new FlowLayout());
 		JTextField textFieldPasswort = new JTextField(20);
 		JLabel labelPasswort = new JLabel("Passwort:");
-		panelPasswortUndLogin.add(labelPasswort);
-		panelPasswortUndLogin.add(textFieldPasswort);
+		panelPasswort.add(labelPasswort);
+		panelPasswort.add(textFieldPasswort);
 
 		// Login
+		JPanel panelButtonLogin = new JPanel();
 		JButton buttonLogin = new JButton("Login");
-		panelPasswortUndLogin.add(buttonLogin);
+		panelButtonLogin.add(buttonLogin);
 
 		// Text
 		JPanel panelText = new JPanel(new BorderLayout());
 		JLabel labelText = new JLabel("Noch kein Benutzerkonto?");
 		panelText.add(labelText);
 
-		// Button
-		JPanel panelRegistrieren = new JPanel(new BorderLayout());
+		// Registrieren
+		JPanel panelButtonRegistrieren = new JPanel(new BorderLayout());
 		JButton buttonRegistrieren = new JButton("Registrieren");
-		panelRegistrieren.add(buttonRegistrieren);
-
-		panelSeite.add(panelTitel);
-		panelSeite.add(panelLogin);
-		panelSeite.add(panelKartennummer);
-		panelSeite.add(panelPasswortUndLogin);
-		panelSeite.add(panelText);
-		panelSeite.add(panelRegistrieren);
-
+		panelButtonRegistrieren.add(buttonRegistrieren);
+		
 		alles.add(panelSeite);
 
 		Container contentPane = getContentPane();
