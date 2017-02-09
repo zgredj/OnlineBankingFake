41b341c25ea98a7a1bdec972f8a2f8f96f39c164
zgredj <zgredj@WS-ZGREDJ.bbcnet-zh.local>
2017-02-09 11:11:32 +0100

    Code sortiert

--- src/gui/Registrieren.java
+++ src/gui/Registrieren.java
@@ -27,76 +27,71 @@ public class Registrieren extends JPanel {
 		JTextField textFieldPlz = new JTextField();
 		JTextField textFieldStrasse = new JTextField();
 		JTextField textFieldHausNr = new JTextField();
-		
+
 		JLabel labelTitel = new JLabel("Neues Benutzerkonto");
-		
-		JLabel stringKartennummer = new JLabel("Kartennummer");
-		JLabel stringPassword = new JLabel("Passwort");
-		JLabel stringPasswordWiederholen = new JLabel("Passwort wiederholen");
-		JLabel stringVorname = new JLabel("Vorname");
-		JLabel stringNachname = new JLabel("Nachname");
-		JLabel stringGeburtsdatum = new JLabel("Geburtsdatum");
-		JLabel stringWohnort = new JLabel("Wohnort");
-		JLabel stringPlz = new JLabel("PLZ");
-		JLabel stringStrasse = new JLabel("Strasse");
-		JLabel stringHausNr = new JLabel("Haus Nr.");
-		JLabel stringLeer = new JLabel(" ");
-		
+
+		JLabel labelKartennummer = new JLabel("Kartennummer");
+		JLabel labelPassword = new JLabel("Passwort");
+		JLabel labelPasswordWiederholen = new JLabel("Passwort wiederholen");
+		JLabel labelVorname = new JLabel("Vorname");
+		JLabel labelNachname = new JLabel("Nachname");
+		JLabel labelGeburtsdatum = new JLabel("Geburtsdatum");
+		JLabel labelWohnort = new JLabel("Wohnort");
+		JLabel labelPlz = new JLabel("PLZ");
+		JLabel labelStrasse = new JLabel("Strasse");
+		JLabel labelHausNr = new JLabel("Haus Nr.");
+		JLabel labelLeer = new JLabel(" ");
+
 		JPanel panelKopfzeile = new JPanel(new BorderLayout());
-		
 		JPanel panelInhalt = new JPanel(new GridLayout(11, 2, 30, 15));
 		JPanel panelInhaltOrt = new JPanel(new BorderLayout());
 		JPanel panelInhaltStrasse = new JPanel(new BorderLayout());
 		JPanel panelOsten = new JPanel(new BorderLayout());
-		
+
 		JButton buttonRegistrieren = new JButton("registrieren");
 
-		panelInhalt.add(stringKartennummer);
+		panelInhalt.add(labelKartennummer);
 		panelInhalt.add(textFieldKartennummer);
-		
-		panelInhalt.add(stringPassword);
+
+		panelInhalt.add(labelPassword);
 		panelInhalt.add(textFieldPassword);
-		
-		panelInhalt.add(stringPasswordWiederholen);
+
+		panelInhalt.add(labelPasswordWiederholen);
 		panelInhalt.add(textFieldPasswordWiederholen);
-		
-		panelInhalt.add(stringVorname);
+
+		panelInhalt.add(labelVorname);
 		panelInhalt.add(textFieldVorname);
-		
-		panelInhalt.add(stringNachname);
+
+		panelInhalt.add(labelNachname);
 		panelInhalt.add(textFieldNachname);
-		
-		panelInhalt.add(stringGeburtsdatum);
+
+		panelInhalt.add(labelGeburtsdatum);
 		panelInhalt.add(textFieldGeburtsdatum);
-		
-		panelInhalt.add(stringWohnort);
+
+		panelInhalt.add(labelWohnort);
 		panelInhalt.add(textFieldWohnort);
-		
-		panelInhalt.add(stringPlz);
+
+		panelInhalt.add(labelPlz);
 		panelInhalt.add(textFieldPlz);
-		
-		panelInhalt.add(stringStrasse);
+
+		panelInhalt.add(labelStrasse);
 		panelInhalt.add(textFieldStrasse);
-		
-		panelInhalt.add(stringHausNr);
+
+		panelInhalt.add(labelHausNr);
 		panelInhalt.add(textFieldHausNr);
-		
-		panelInhalt.add(stringLeer);
+
+		panelInhalt.add(labelLeer);
 		panelInhalt.add(buttonRegistrieren);
 
-		
-		
 		labelTitel.setFont(new Font("Arial", Font.PLAIN, 55));
-		
+
 		add(panelInhalt, BorderLayout.CENTER);
 		add(panelKopfzeile, BorderLayout.NORTH);
-		
-		
-		
+
 		panelKopfzeile.add(labelTitel, BorderLayout.NORTH);
 		panelKopfzeile.add(panelInhalt, BorderLayout.WEST);
 		panelKopfzeile.add(panelOsten, BorderLayout.EAST);
 		panelInhalt.setBorder(BorderFactory.createEmptyBorder(40, 30, 10, 0));
-		
+
 	}
 }
