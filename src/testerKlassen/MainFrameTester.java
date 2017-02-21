a9513f97322fb0bfd1230a92fe1a2c89de79c850
zkappa <zkappa@WS-ZKAPPA.bbcnet-zh.local>
2017-02-21 09:04:00 +0100

    Direkt in Login

--- src/testerKlassen/MainFrameTester.java
+++ src/testerKlassen/MainFrameTester.java
@@ -1,5 +1,6 @@
 package testerKlassen;
 
+import gui.LayoutEingeloggt;
 import gui.MainFrame;
 
 public class MainFrameTester {
@@ -7,6 +8,9 @@ public class MainFrameTester {
 	public static void main(String[] args) {
 		
 		MainFrame br1 = new MainFrame();
+		br1.getContentPane().removeAll();
+		br1.getContentPane().add(new LayoutEingeloggt(br1, "Vorname", "nachname", 1));
+		br1.getContentPane().revalidate();
 	}
 
 }
